9abe1f1d84fea32707fd4f5a373deb3a994494fc
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-05-28 20:34:06 -0300

    Add DomainApiDataSource

---
+++ image-labeling-system/image-labeling-android/app/src/main/java/br/com/argmax/imagelabeling/service/remote/domain/DomainApiDataSource.kt
@@ -0,0 +1,13 @@
+package br.com.argmax.imagelabeling.service.remote.domain
+
+import br.com.argmax.imagelabeling.domain.entities.Domain
+import io.reactivex.Observable
+import retrofit2.http.GET
+
+interface DomainApiDataSource {
+
+    @GET("domains/")
+    fun domainList(): Observable<List<Domain>>
+
+
+}
