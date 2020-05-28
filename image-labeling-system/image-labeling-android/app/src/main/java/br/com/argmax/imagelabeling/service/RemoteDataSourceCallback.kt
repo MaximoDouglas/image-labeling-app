8d13d6f6b397c6dcc2b1fdef4d02eecd62c1b82f
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-05-28 20:09:39 -0300

    Add base RemoteDataSourceCallback

---
+++ image-labeling-system/image-labeling-android/app/src/main/java/br/com/argmax/imagelabeling/service/RemoteDataSourceCallback.kt
@@ -0,0 +1,12 @@
+package br.com.argmax.imagelabeling.service
+
+interface RemoteDataSourceCallback<R> {
+
+    fun onSuccess(response: R)
+
+    fun isLoading(isLoading: Boolean)
+
+    fun onError(errorMessage: String)
+
+
+}
