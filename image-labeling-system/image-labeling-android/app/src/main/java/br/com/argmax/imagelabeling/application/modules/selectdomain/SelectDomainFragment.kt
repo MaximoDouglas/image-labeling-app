ff9735260df2edc646d3bd70d80481e87f6ac942
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-07-06 23:45:51 -0300

    Add select domain fragment base

--- image-labeling-system/image-labeling-android/app/src/main/java/br/com/argmax/imagelabeling/application/modules/selectdomain/SelectDomainFragment.kt
+++ image-labeling-system/image-labeling-android/app/src/main/java/br/com/argmax/imagelabeling/application/modules/selectdomain/SelectDomainFragment.kt
@@ -1,4 +1,28 @@
 package br.com.argmax.imagelabeling.application.modules.selectdomain
 
-class SelectDomainFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import br.com.argmax.imagelabeling.R
+import br.com.argmax.imagelabeling.databinding.SelectDomainFragmentBinding
+
+class SelectDomainFragment : Fragment() {
+
+    private var mBinding: SelectDomainFragmentBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.select_domain_fragment, container, false)
+
+        return mBinding?.root
+    }
+
 }
