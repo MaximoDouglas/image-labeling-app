ce3f5e6ff539ab454f50bb93d62f2c3ce5bf6caf
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-07-08 23:43:43 -0300

    Add dagger dependencies

--- image-labeling-system/image-labeling-android/app/src/main/java/br/com/argmax/imagelabeling/application/injections/CoroutineContextProvider.kt
+++ image-labeling-system/image-labeling-android/app/src/main/java/br/com/argmax/imagelabeling/application/injections/CoroutineContextProvider.kt
@@ -1,6 +1,7 @@
 package br.com.argmax.imagelabeling.application.injections
 
 import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
 class CoroutineContextProvider @Inject constructor() {
