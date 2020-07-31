eee0b07ed57f1e5036b9a0f2d456b668fd3fc7ac
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-07-31 00:55:58 -0300

    Add DomainCardComponent implementation class

--- app/src/main/java/br/com/argmax/imagelabeling/application/components/domaincard/DomainCardComponent.kt
+++ app/src/main/java/br/com/argmax/imagelabeling/application/components/domaincard/DomainCardComponent.kt
@@ -1,5 +1,25 @@
 package br.com.argmax.imagelabeling.application.components.domaincard
 
-class DomainCardComponent {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.databinding.DataBindingUtil
+import br.com.argmax.imagelabeling.R
+import br.com.argmax.imagelabeling.databinding.DomainCardComponentBinding
+import br.com.argmax.imagelabeling.service.entities.Domain
+import com.google.android.material.card.MaterialCardView
+
+class DomainCardComponent @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyle: Int = 0
+) : MaterialCardView(context, attrs, defStyle) {
+
+    private var mBinding: DomainCardComponentBinding? = DataBindingUtil.inflate(
+        LayoutInflater.from(context), R.layout.domain_card_component, this, true
+    )
+
+    fun setDomain(domain: Domain) {
+        mBinding?.domain = domain
+        mBinding?.executePendingBindings()
+    }
 
 }
