2f2e3621a95efde1c5b8c5843f43e50be3275c36
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-08-03 15:31:13 -0300

    Add AppComponent interface and its conection with BaseApplication

--- app/src/main/java/br/com/argmax/imagelabeling/BaseApplication.kt
+++ app/src/main/java/br/com/argmax/imagelabeling/BaseApplication.kt
@@ -1,13 +1,13 @@
 package br.com.argmax.imagelabeling
 
+import br.com.argmax.imagelabeling.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
-
 
 class BaseApplication : DaggerApplication() {
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication>? {
-        return null
+        return DaggerAppComponent.builder().application(this).build()
     }
 
 }
---
+++ app/src/main/java/br/com/argmax/imagelabeling/di/AppComponent.kt
@@ -0,0 +1,27 @@
+package br.com.argmax.imagelabeling.di
+
+import android.app.Application
+import br.com.argmax.imagelabeling.BaseApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+
+@Component(
+    modules = [
+        AndroidSupportInjectionModule::class
+    ]
+)
+interface AppComponent : AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): AppComponent
+
+    }
+
+}
