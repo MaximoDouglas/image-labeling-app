997465803f7cca3f1ce4cd7e83fa2258d9a01904
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-08-04 23:34:48 -0300

    Add singleton modifier

--- app/src/main/java/br/com/argmax/imagelabeling/di/AppModule.kt
+++ app/src/main/java/br/com/argmax/imagelabeling/di/AppModule.kt
@@ -14,16 +14,19 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 object AppModule {
 
+    @Singleton
     @Provides
     @JvmStatic
     fun provideDomainRemoteDataSource(domainApiDataSource: DomainApiDataSource): DomainRemoteDataSource {
         return DomainRemoteDataSourceImpl(domainApiDataSource)
     }
 
+    @Singleton
     @Provides
     @JvmStatic
     fun provideDomainApiDataSource(retrofit: Retrofit): DomainApiDataSource {
@@ -35,6 +38,7 @@ object AppModule {
 @Module
 object RemoteServiceModule {
 
+    @Singleton
     @Provides
     @JvmStatic
     fun provideRetrofitService(gsonBuilder: Gson, httpClient: OkHttpClient.Builder): Retrofit {
@@ -46,12 +50,14 @@ object RemoteServiceModule {
             .build()
     }
 
+    @Singleton
     @Provides
     @JvmStatic
     fun provideGsonBuilder(): Gson {
         return GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create()
     }
 
+    @Singleton
     @Provides
     @JvmStatic
     fun provideOkHttpClientBuilder(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient.Builder {
@@ -61,6 +67,7 @@ object RemoteServiceModule {
         return builder
     }
 
+    @Singleton
     @Provides
     @JvmStatic
     fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
