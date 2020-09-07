3182f1cec5c01a7e3fa0faa0f93890d237cabffc
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-09-07 20:53:01 -0300

    Add domain details unwraple to the Domain Detail fragment

--- app/src/main/java/br/com/argmax/imagelabeling/application/modules/domaindetail/DomainDetailFragment.kt
+++ app/src/main/java/br/com/argmax/imagelabeling/application/modules/domaindetail/DomainDetailFragment.kt
@@ -5,13 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil.inflate
+import androidx.navigation.fragment.navArgs
 import br.com.argmax.imagelabeling.R
 import br.com.argmax.imagelabeling.databinding.DomainDetailFragmentBinding
+import br.com.argmax.imagelabeling.service.entities.Domain
 import dagger.android.support.DaggerFragment
 
 class DomainDetailFragment : DaggerFragment() {
 
     private var mBinding: DomainDetailFragmentBinding? = null
+    private var mDomain: Domain? = null
+
+    private val args: DomainDetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,7 +26,25 @@ class DomainDetailFragment : DaggerFragment() {
         super.onCreateView(inflater, container, savedInstanceState)
         mBinding = inflate(inflater, R.layout.domain_detail_fragment, container, false)
 
+        unwrapArgs()
+
         return mBinding?.root
+    }
+
+    private fun unwrapArgs() {
+        mDomain = args.domain
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (mDomain != null) {
+            val domainId = mDomain?.id ?: 0
+            val domainDescription = mDomain?.description ?: ""
+
+            mBinding?.domainDetailsFragmentDomainIdTextView?.text = domainId.toString()
+            mBinding?.domainDetailsFragmentDomainDescriptionTextView?.text = domainDescription
+        }
     }
 
 }
