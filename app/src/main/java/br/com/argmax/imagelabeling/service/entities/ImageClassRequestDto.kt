9702f7650ad968e78732d70877f1783a5cd64424
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-09-09 03:56:59 -0300

    Update ImageClassRequestDto

--- app/src/main/java/br/com/argmax/imagelabeling/service/entities/ImageClassRequestDto.kt
+++ app/src/main/java/br/com/argmax/imagelabeling/service/entities/ImageClassRequestDto.kt
@@ -1,8 +1,11 @@
 package br.com.argmax.imagelabeling.service.entities
 
+import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 data class ImageClassRequestDto(
+    val id: Int,
     val name: String,
-    val domain_id: String
+    @SerializedName("domain_id")
+    val domainId: Int
 ) : Serializable
