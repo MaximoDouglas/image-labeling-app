579033f7955c664a20e71372b3e7a8367ff0909b
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-09-17 23:50:27 -0300

    Add ImageClass adapter, card components and view holder

---
+++ app/src/main/java/br/com/argmax/imagelabeling/application/components/imageclasscard/ImageClassCardComponent.kt
@@ -0,0 +1,27 @@
+package br.com.argmax.imagelabeling.application.components.imageclasscard
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.databinding.DataBindingUtil
+import br.com.argmax.imagelabeling.R
+import br.com.argmax.imagelabeling.databinding.DomainCardComponentBinding
+import br.com.argmax.imagelabeling.databinding.ImageClassCardComponentBinding
+import br.com.argmax.imagelabeling.service.entities.domain.DomainResponseDto
+import br.com.argmax.imagelabeling.service.entities.imageclass.ImageClassResponseDto
+import com.google.android.material.card.MaterialCardView
+
+class ImageClassCardComponent @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyle: Int = 0
+) : MaterialCardView(context, attrs, defStyle) {
+
+    private var mBinding: ImageClassCardComponentBinding? = DataBindingUtil.inflate(
+        LayoutInflater.from(context), R.layout.image_class_card_component, this, true
+    )
+
+    fun setImageClassResponseDto(imageClassResponseDto: ImageClassResponseDto) {
+        mBinding?.imageClassResponseDto = imageClassResponseDto
+        mBinding?.executePendingBindings()
+    }
+
+}
---
+++ app/src/main/java/br/com/argmax/imagelabeling/application/modules/domaindetail/adapters/ImageClassAdapter.kt
@@ -0,0 +1,57 @@
+package br.com.argmax.imagelabeling.application.modules.domaindetail.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import br.com.argmax.imagelabeling.R
+import br.com.argmax.imagelabeling.application.modules.domaindetail.adapters.ImageClassAdapter.ImageClassCardViewHolder
+import br.com.argmax.imagelabeling.databinding.ImageClassCardViewHolderBinding
+import br.com.argmax.imagelabeling.service.entities.imageclass.ImageClassResponseDto
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.image_class_card_view_holder.*
+
+class ImageClassAdapter : Adapter<ImageClassCardViewHolder>() {
+
+    private var mData: List<ImageClassResponseDto> = listOf()
+
+    fun replaceData(imageClassResponseDtoList: List<ImageClassResponseDto>) {
+        mData = imageClassResponseDtoList
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageClassCardViewHolder {
+        val imageClassViewHolderBinding: ImageClassCardViewHolderBinding =
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.domain_card_view_holder,
+                parent,
+                false
+            )
+
+        return ImageClassCardViewHolder(imageClassViewHolderBinding.root)
+    }
+
+    override fun getItemCount(): Int {
+        return mData.size
+    }
+
+    override fun onBindViewHolder(holder: ImageClassCardViewHolder, position: Int) {
+        holder.updateData(mData[position])
+    }
+
+    inner class ImageClassCardViewHolder(
+        itemView: View
+    ) : RecyclerView.ViewHolder(itemView), LayoutContainer {
+
+        override val containerView: View?
+            get() = itemView
+
+        fun updateData(imageClassResponseDto: ImageClassResponseDto) {
+            imageClassCardComponent.setImageClassResponseDto(imageClassResponseDto)
+        }
+    }
+
+}
