06e2a12b09cee0d075df5cd5c9a6bad7bdc4390b
MaximoDouglas <douglas.h.maximo@gmail.com>
2021-04-14 20:53:40 -0300

    Add behavior to the image class delete button

--- app/src/main/java/br/com/argmax/imagelabeling/service/remote/image/ImageRemoteDataSourceImpl.kt
+++ app/src/main/java/br/com/argmax/imagelabeling/service/remote/image/ImageRemoteDataSourceImpl.kt
@@ -4,11 +4,11 @@ import br.com.argmax.imagelabeling.service.entities.image.ImageRequestDto
 import br.com.argmax.imagelabeling.service.entities.image.ImageResponseDto
 
 class ImageRemoteDataSourceImpl(
-    private val mImageClassApiDataSource: ImageApiDataSource
+    private val mImageApiDataSource: ImageApiDataSource
 ) : ImageRemoteDataSource {
 
     override suspend fun sendImage(imageRequestDto: ImageRequestDto): ImageResponseDto {
-        return mImageClassApiDataSource.sendImage(imageRequestDto)
+        return mImageApiDataSource.sendImage(imageRequestDto)
     }
 
 }
